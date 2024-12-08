4332d513c125403b5f6c05d8e048abcf40cb5f51
breadNbutter42 <97899677+breadNbutter42@users.noreply.github.com>
2024-12-08 03:22:05 -0500

    Update BroTokenWithPresale.sol add emergency withdrawal

--- BRO/BroTokenWithPresale.sol
+++ BRO/BroTokenWithPresale.sol
@@ -99,7 +99,7 @@ interface IERC721Token { //Generic ability to transfer out NFTs accidentally sen
 
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity 0.8.24;
+pragma solidity 0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
