3b71b9322a108a85dac40b4abd847bab31061792
breadNbutter42 <97899677+breadNbutter42@users.noreply.github.com>
2024-12-12 04:37:01 -0500

    Update BroTokenWithPresale.sol update import comments

--- BRO/BroTokenWithPresale.sol
+++ BRO/BroTokenWithPresale.sol
@@ -86,9 +86,9 @@ interface ITJUniswapV2Router01 {
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity 0.8.28;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; //OpenZeppelin Contracts (last updated v5.0.0+)
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol"; //OpenZeppelin Contracts (last updated v5.0.0+)
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol"; //OpenZeppelin Contracts (last updated v5.1.0)
 
 contract BroTokenWithPresale is ERC20, ERC20Permit, ReentrancyGuard {
 
